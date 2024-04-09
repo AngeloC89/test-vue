1e7fe99d61892137882e471927e3d0b4a0885570
AngeloC89 <Angelociulla89@gmail.com>
2024-04-09 15:24:43 +0200

    print on DOM an h1 "Vite Hello World"

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+<h1>Vite Hello World</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+h1{
+    color: green;
+}
+</style>
